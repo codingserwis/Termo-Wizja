b7b5a5130ebd7c669000a383b4ed603f879dd79b
CodingSerwis <maciej.lehrer@codingserwis.pl>
2017-10-22 00:36:33 +0200

    work in progress

--- home.php
+++ home.php
@@ -4,18 +4,49 @@
  */
     get_header();
 
-    $test = get_field('termovision_title');
 ?>
 <main>
-    <section>
-
-    </section>
-    <section>
+    <section class="main-section-bcg" style="min-height: 75vh; height: 75vh;">
         <div class="container">
             <div class="row">
-                <div class="col-xl-5">
-
+                <div class="col-xl-7">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <h1 class="text-uppercase text-white">
+                                profesjonalne
+                            </h1>
+                            <h1 class="text-uppercase text-orange">
+                                badania
+                            </h1>
+                            <h1 class="text-uppercase text-orange font-weight-bold">
+                                <strong>termowizyjne</strong>
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-12">
+                            zadzwoń zapytaj o wycenę
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xl-12">
+                            666 666 666
+                        </div>
+                    </div>
                 </div>
+                <div class="col-xl-5">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <img class="img-fluid" src="localhost:3000/WP-4_8_1/wp-content/themes/TermoWizja/assets/img/main-termo-bcg.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-border termovision-bcg">
+        <div class="container">
+            <div class="row justify-content-end">
                 <div class="col-xl-7">
                     <div class="row">
                         <div class="col-xl-12">
@@ -42,9 +73,9 @@
             </div>
         </div>
     </section> <!-- end of termovision section -->
-    <section>
+    <section class="electrict-bcg section-border">
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-start">
                 <div class="col-xl-7">
                     <div class="row">
                         <div class="col-xl-12">
@@ -64,17 +95,58 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-5">
-    
-                </div>
             </div>
         </div>
     </section> <!-- end of electrical section -->
-    <section>
-
+    <section class="light-bcg section-border">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-xl-7">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <h1 class="section-main-title font-weight-light">
+                                <?php the_field('light_title'); ?>
+                            </h1>
+                        </div>
+                        <div class="col-xl-12">
+                            <p class="my-4 section-content">
+                                <?php the_field('light_content'); ?>
+                            </p>
+                        </div>
+                        <div class="col-xl-12">
+                            <h2 class="section-link font-weight-light">
+                                <?php the_field('light_link'); ?>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section> <!-- end of light mesurment section-->
-    <section>
-
+    <section class="alarm-systems section-border">
+        <div class="container">
+                <div class="row">
+                    <div class="col-xl-7">
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <h1 class="section-main-title font-weight-light">
+                                    <?php the_field('security_title'); ?>
+                                </h1>
+                            </div>
+                            <div class="col-xl-12">
+                                <p class="my-4 section-content">
+                                    <?php the_field('security_content'); ?>
+                                </p>
+                            </div>
+                            <div class="col-xl-12">
+                                <h2 class="section-link font-weight-light">
+                                    <?php the_field('security_link'); ?>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </section> <!-- end of alarms section -->
 </main>
 
--- prod/footer.php
+++ prod/footer.php
@@ -1,4 +1,36 @@
-        <footer>
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <address>
+                            <h1 class="font-weight-light">
+                                Pomiary elektryczne i termowizyjne
+                            </h1>
+                            <h4 class="font-weight-light">
+                                Artur Maliszewski
+                            </h4>
+                            <h4 class="font-weight-light">
+                                    Kępa ul. Miła 5
+                            </h4>
+                            <ul class="mt-3">
+                                <li>
+                                    <a href="">
+                                        <i></i>666 666 666
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                    <i></i> kontakt@gmail.com
+                                    </a>
+                                </li>
+                            </ul>
+                        </address>
+                    </div>
+                    <div class="col-xl-6 d-flex justify-content-end align-items-start">
+                        <img class="img" src="http://localhost:3000/WP-4_8_1/wp-content/themes/TermoWizja/assets/img/home.svg" alt="">
+                    </div>
+                </div>
+            </div>
             <?php wp_footer(); ?>
         </footer> <!-- end of footer -->
     </body> <!-- end of body -->
