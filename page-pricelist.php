1866e0ea10629118ea645e54d8754d161e3b1075
CodingSerwis <maciej.lehrer@codingserwis.pl>
2017-10-27 00:13:07 +0200

    work in progres

--- page-pricelist.php
+++ page-pricelist.php
@@ -5,4 +5,16 @@
     get_header();
 ?>
 
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <h1>
+                    Strona w przygotowaniu
+                </h1>  
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>
--- prod/header.php
+++ prod/header.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php bloginfo('language'); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body>
-    <?php echo '<header class="' . (is_page_template( 'prod/home.php' ) ? 'header' : 'header white') . '">'; ?>
+    <?php echo '<header class="' . (is_page_template( 'home.php' ) ? 'header' : 'header white') . '">'; ?>
         <div class="container">
             <div class="row">
                 <div class="col-xl-12">
