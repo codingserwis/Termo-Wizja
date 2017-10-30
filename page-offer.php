cbbfdc06e547cec0cdfabd46e67e755e3033c696
CodingSerwis <maciej.lehrer@codingserwis.pl>
2017-10-30 23:30:08 +0100

    offer rwd

--- page-offer.php
+++ page-offer.php
@@ -9,16 +9,16 @@
     <section class="basic-section">
         <div class="container">
             <div class="row">
-                <div class="col-xl-12">
+                <div class="col-12">
                     <div class="row">
-                        <div class="col-xl-7">
+                        <div class="col-12 col-xl-7">
                             <h2 class="section-link font-weight-light">
                                 <?php the_field('page_title'); ?>
                             </h2>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xl-8 offer-page">
+                        <div class="col-12 col-xl-8 offer-page">
                             <?php if ( have_posts()) : while (have_posts()) : the_post(); ?>
                                 <div class="page-content">
                                     <?php the_content(); ?>
@@ -39,7 +39,7 @@
                 </div>
             </div>
             <div class="row mt-4">
-                <div class="col-xl-12">
+                <div class="col-12">
                     <div class="row justify-content-end">
                         <div class="col-auto">
                             <a href="<?php bloginfo('url'); ?>" class="hero-phone btn btn-primary badge badge-pill py-3 px-4 d-flex align-items-center text-uppercase">wróć</a>
